ba4bf482ebca284e5978869326685dfca404d2c0
valerianguivarch <vguivarch@appstud.com>
2022-07-12 21:41:28 +0200

    FEAT remove auth 2

--- src/main/domain/providers/IAuthenticationProvider.ts
+++
@@ -1,8 +0,0 @@
-import { AuthRequest } from '../models/auth/AuthRequest'
-import { AuthType } from '../models/auth/AuthType'
-
-export interface IAuthenticationProvider {
-  verifyLogin(request: AuthRequest): Promise<boolean>
-
-  supports(type: AuthType): boolean
-}
