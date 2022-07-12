f25c2a6565faa90216a19dceb783ddb8122890f5
valerianguivarch <vguivarch@appstud.com>
2022-07-12 21:43:37 +0200

    FEAT remove auth 3

--- src/main/domain/providers/ITokenProvider.ts
+++
@@ -1,9 +0,0 @@
-import { Authentication } from '../models/auth/Authentication'
-import { Authority } from '../models/auth/Authority'
-import { JWTToken } from '../models/auth/JWTToken'
-
-export interface ITokenProvider {
-  generateTokens(p: { accountId: string; authority: Authority; secret: string }): Promise<JWTToken>
-  unpackToken(accessToken: string): Promise<Authentication>
-  verifyToken(accessToken: string, secret: string): Promise<void>
-}
